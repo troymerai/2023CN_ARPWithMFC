fcdb03b365e1fb172caaae88c52339a83e2bf576
troymerai <16alexandros57@gmail.com>
2023-11-05 00:20:35 +0900

    MFC 구현

---
+++ computernetwork_ARP/resource.h
@@ -0,0 +1,35 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// computernetworkARP.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_COMPUTERNETWORK_ARP_DIALOG  102
+#define IDR_MAINFRAME                   128
+#define IDC_IPADDRESS1                  1007
+#define IDC_BUTTON1                     1008
+#define IDC_BUTTON4                     1009
+#define IDC_IPADDRESS2                  1010
+#define IDC_NETADDRESS1                 1012
+#define IDC_COMBOBOXEX1                 1015
+#define IDC_NETADDRESS2                 1016
+#define IDC_BUTTON3                     1019
+#define IDC_BNT_LIST_ITEM_DEL           1022
+#define IDC_BNT_LIST_ALL_DEL            1023
+#define IDC_BNT_ARP_DEL                 1024
+#define IDC_BNT_ARP_ADD                 1025
+#define IDC_LIST                        1039
+#define IDC_LIST8                       1040
+#define IDC_LIST_ARP                    1040
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1041
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
